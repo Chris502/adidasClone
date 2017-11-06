2a8ddae627acc304ccfd30795673c8e7b6e8c356
christopher anderson <canderson0289@gmail.com>
2017-11-06 17:12:54 -0500

    got dropdown menus working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
 import logo from './adidas-black-vector-logo-400x400.png'
 
-class Header extends Component{
-    constructor(){
+class Header extends Component {
+    constructor() {
         super();
         this.state = {}
     }
-render(){
-    return(
-        <div className="header-main">
-        
-        <img src={logo} className="logo-head"></img>
-        <div className='login-bar'>
-            <p className='tracker'>Track Your Order</p>
-            |
+    render() {
+        return (
+            <div className="header-main">
+
+                <img src={logo} className="logo-head"></img>
+                <div className='login-bar'>
+                    <p className='tracker'>Track Your Order</p>
+                    |
             <p className='news-letter'>NEWSLETTER SIGNUP</p>
-            <p className='login-link'>Login</p>
-        </div>
-        <div className='menu-div'>
-        <ul className='menu'>
-            <li className='mens-box'><strong>MEN</strong></li>
-            <li><strong>WOMEN</strong></li>
-            <li><strong>KIDS</strong></li>
-        </ul>
-        </div>
-        </div>
-    )
-}
+                    <p className='login-link'>Login</p>
+                </div>
+                <div className='menu-div'>
+                    <ul className='menu'>
+                        <li className='mens-box'><strong>MEN</strong>
+                            <div className='mens-items'>
+                                <p>test</p>
+                            </div></li>
+                        <li className='womens-box'><strong>WOMEN</strong>
+                            <div className='womens-items'>
+                                <p>test1</p>
+                            </div>
+                        </li>
+                        <li className='kids-box'><strong>KIDS</strong>
+                            <div className='kids-items'>
+                                <p>test2</p>
+                            </div>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+
+            </div>
+        )
+    }
 }
 export default Header
