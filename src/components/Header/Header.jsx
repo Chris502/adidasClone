cd51c012ff4ded93e245b3577037609b79c83ef9
christopher anderson <canderson0289@gmail.com>
2017-11-08 11:41:04 -0500

    finished mens tab in menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,14 +26,90 @@ class Header extends Component {
                         <li className='mens-box'><strong>MEN</strong>
                             <div className='mens-items'>
                                 <div style={style}>
-                                <ul>
+                                <ul className='featured-list'>
                                     <h3>Featured</h3>
+                                    <li>New Arrivals</li>
+                                    <li>Best Sellers</li>
+                                    <li>Release Dates</li>
+                                    <li><strong>Sale - New Markdowns Added</strong></li>
+                                    <li><br/></li>
+                                    <li>EQT</li>
+                                    <li>Tubular</li>
+                                    <li>NMD</li>
+                                    <li>UltraBOOST</li>
+                                    <li>adidas x Reigning Champ</li>
+                                    <li>Winter Layers</li>
                                 </ul>
                                 </div>
-                                <div style={style}>b</div>
-                                <div style={style}>c</div>
-                                <div style={style}>d</div>
-                                <div style={style}>f</div>
+                                <div style={style}>
+                                <ul className='featured-list shoe-list'>
+                                    <h3>Shoes</h3>
+                                    <li>Originals</li>
+                                    <li>Running</li>
+                                    <li>Soccer</li>
+                                    <li>Basketball</li>
+                                    <li>Training</li>
+                                    <li>Football</li>
+                                    <li>Sandals & Slides</li>
+                                    <li>Outdoor</li>
+                                    <li>Tennis</li>
+                                    <li>Skateboard</li>
+                                    <li>Baseball</li>
+                                    <li>Golf</li>
+                                    <li>Customize with mdidas</li>
+                                </ul>
+                                </div>
+                                <div style={style}>
+                                <ul className='featured-list apparel'>
+                                    <h3>Apparel</h3>
+                                    <li>Pants</li>
+                                    <li>Hoodies & Sweatshirts</li>
+                                    <li>Jackets</li>
+                                    <li>Track Suit & Warm Ups</li>
+                                    <li>Short Sleeve Shirts</li>
+                                    <li>Long Sleeve Shirts</li>
+                                    <li>Polos</li>
+                                    <li>Graphic T-Shirts</li>
+                                    <li>Jerseys</li>
+                                    <li>Tights</li>
+                                    <li>Shorts</li>
+                                    <li>Tank Tops</li>
+                                    <li>Underwear</li>
+                                </ul>
+                                </div>
+                                <div style={style}>
+                                <ul className='featured-list accessories'>
+                                    <h3>Accessories</h3>
+                                    <li>Bags & Backpacks</li>
+                                    <li>Hats & Beanies</li>
+                                    <li>Socks</li>
+                                    <li>Phonecases</li>
+                                    <li>Watches</li>
+                                    <li>Sunglasses</li>
+                                    <li>Balls</li>
+                                    <li>Gloves</li>
+                                    <li>Scarves</li>
+                                    <li>All Accessories</li>
+                                </ul>
+                                </div>
+                                <div style={style}>
+                                <ul className='featured-list sports'>
+                                    <h3>Sports</h3>
+                                    <li>Running</li>
+                                    <li>Soccer</li>
+                                    <li>Basketball</li>
+                                    <li>Training</li>
+                                    <li>Football</li>
+                                    <li>Outdoor</li>
+                                    <li>Tennis</li>
+                                    <li>Skateboarding</li>
+                                    <li>Baseball</li>
+                                    <li>Weightligting</li>
+                                    <li>Hockey</li>
+                                    <li>Golf</li>
+                                    <li>Lacrosse</li>
+                                </ul>
+                                </div>
                             </div></li>
                         <li className='womens-box'><strong>WOMEN</strong>
                             <div className='womens-items'>
